9c4debb1b5eb3be68d8f93e3f63e4a1ede8021bc
docs <shuang20@bu.edu>
2024-04-17 16:43:36 -0400

    update News page header

--- app/src/main/java/com/example/codecompany_cryptotracker/presentation/NewsScreen.kt
+++ app/src/main/java/com/example/codecompany_cryptotracker/presentation/NewsScreen.kt
@@ -9,16 +9,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.codecompany_cryptotracker.data.model.CoinNewsViewModel
@@ -26,6 +31,7 @@ import com.example.codecompany_cryptotracker.network.CoinReposImp
 import com.example.codecompany_cryptotracker.network.RetrofitNewsInstance
 import java.net.URLEncoder
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NewsList(navController: NavController) {
@@ -35,38 +41,59 @@ fun NewsList(navController: NavController) {
     }
     var coinNews = newsViewModel.products.collectAsState().value.articles
 
-    LazyColumn {
-        items(coinNews.size) { index ->
-            val article = coinNews[index]
-            Card(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .clickable {
-                        // Use the launcher to open the news URL in a browser
-                        navController.navigate("webView/${URLEncoder.encode(article.url, "UTF-8")}")
-                    }
-            ) {
-                Column {
-                    // Load and display the image from the URL
-                    article.imageUrl?.let { imageUrl ->
-                        AsyncImage(
-                            model = ImageRequest.Builder(LocalContext.current)
-                                .data(imageUrl)
-                                .crossfade(true)
-                                .build(),
-                            contentDescription = "News Image",
-                            modifier = Modifier
-                                .height(200.dp)
-                                .fillMaxWidth(),
-                            contentScale = ContentScale.Crop
-                        )
-                    }
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        Text(text = article.title, style = MaterialTheme.typography.titleLarge)
-                        Text(text = article.description ?: "", style = MaterialTheme.typography.bodyLarge)
+    Column {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Crypto News",
+                    textAlign = TextAlign.Center, // Aligning text to the center
+                    modifier = Modifier.fillMaxWidth() // Making sure the text takes full width
+                )
+            },
+            // backgroundColor = MaterialTheme.colors.primary
+        )
+        LazyColumn {
+            items(coinNews.size) { index ->
+                val article = coinNews[index]
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clickable {
+                            // Use the launcher to open the news URL in a browser
+                            navController.navigate("webView/${URLEncoder.encode(article.url, "UTF-8")}")
+                        }
+                ) {
+                    Column {
+                        // Load and display the image from the URL
+                        article.imageUrl?.let { imageUrl ->
+                            AsyncImage(
+                                model = ImageRequest.Builder(LocalContext.current)
+                                    .data(imageUrl)
+                                    .crossfade(true)
+                                    .build(),
+                                contentDescription = "News Image",
+                                modifier = Modifier
+                                    .height(200.dp)
+                                    .fillMaxWidth(),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(text = article.title, style = MaterialTheme.typography.titleLarge)
+                            Text(text = article.description ?: "", style = MaterialTheme.typography.bodyLarge)
+                        }
                     }
                 }
             }
         }
     }
+
+
+}
+@Preview(showBackground = true)
+@Composable
+fun Previewfun() {
+//    DeveloperSection()
+    var navController = rememberNavController()
+    NewsList(navController = navController)
 }
